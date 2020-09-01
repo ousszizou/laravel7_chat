3786c75802103dc3609ebbd1357c00d77c3a2764
algorithm <odjaidri@gmail.com>
2020-09-01 10:44:26 +0100

    branch[app1_authorization_gates]

--- app/User.php
+++ app/User.php
@@ -6,6 +6,8 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+use App\Post;
+
 class User extends Authenticatable
 {
     use Notifiable;
@@ -36,4 +38,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function posts() {
+        return $this->hasMany(Post::class);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,4 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
+Route::get('/posts', 'PostController@index')->middleware('auth');
