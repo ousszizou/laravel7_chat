cd85386e671ac28b0f3ffabf0f2f1a9bf53fc0db
algorithm <odjaidri@gmail.com>
2020-09-21 15:45:13 +0100

    branch[app1_email_verification]

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Auth::routes();
+Auth::routes(['verify' => true]);
 
 Route::get('/home', 'HomeController@index')->name('home');
 
